0018fbb894b47eda31f5291391511612584285dd
dvaer <dvaerkagama@gmail.com>
2024-08-15 02:04:45 +0300

    add_class

---
+++ ZoFo/GameCore/GUI/MainMenuGUI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Xml;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonogameLibrary.UI.Base;
+using MonogameLibrary.UI.Elements;
+
+namespace ZoFo.GameCore.GUI;
+
+public class MainMenuGUI : AbstractGUI
+{
+    protected override void CreateUI()
+    {
+        // int width = AppManager.Instance.inGameHUDHelperResolution.X;
+        // int height = AppManager.Instance.inGameHUDHelperResolution.Y;
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        
+    }
+}
