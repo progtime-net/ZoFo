c2d070cd8de769e7ce1e1438d820a6325ee0fab9
unknown <dimasima2908@gmail.com>
2024-08-15 01:07:51 +0300

    2element

---
+++ ZoFo/GameCore/GameObjects/LootData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZoFo.GameCore.GameObjects
+{
+    internal class LootData
+    {
+        public void AddLoot(object lootObject, int quantity)
+        {
+
+        }
+    }
+}
