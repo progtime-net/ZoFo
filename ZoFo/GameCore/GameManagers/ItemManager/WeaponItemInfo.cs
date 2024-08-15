475439a3af9577c07d8ef6bbdb7ce2c3cee29d62
Lev <GitHub16102010@gmail.com>
2024-08-15 17:28:27 +0300

    add register to monster 2

--- ZoFo/GameCore/GameManagers/ItemManager/WeaponItemInfo.cs
+++ ZoFo/GameCore/GameManagers/ItemManager/WeaponItemInfo.cs
@@ -10,5 +10,9 @@ namespace ZoFo.GameCore.GameManagers.ItemManager
     {
         //поля
         float damage;
+
+        public WeaponItemInfo(string tag) : base(tag)
+        {
+        }
     }
 }
