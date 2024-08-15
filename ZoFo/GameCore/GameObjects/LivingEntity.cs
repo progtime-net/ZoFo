f3c2220769f4d1a4bc47fae2271d30495f245a02
unknown <dimasima2908@gmail.com>
2024-08-15 17:17:25 +0300

    smallEdit

--- ZoFo/GameCore/GameObjects/LivingEntity.cs
+++ ZoFo/GameCore/GameObjects/LivingEntity.cs
@@ -1,30 +1,20 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace ZoFo.GameCore.GameObjects;
 public class LivingEntity : Entity
 {
-    public bool isOnGround = true;
     public Vector2 velocity;
-    public Vector2 acceleration;
-    public Vector2 Acceleration { get; private set; }
-    public LivingEntity(Vector2 position) : base(position)
+    public Vector2 position;
+
+    public void TextureLoad(SpriteBatch spriteBatch)
     {
-        acceleration = new Vector2(0, 30);
-    }
-    public override void SetPosition(Vector2 position)
-    {
-        _pos = position;
 
     }
-    public override void Update(GameTime gameTime)
-    {
-        base.Update(gameTime);
-    }
-    public virtual void StartCicycleAnimation(string animationName)
-    {
-        if (GraphicsComponent.GetCurrentAnimation != animationName)
-        {
-            GraphicsComponent.StartAnimation(animationName);
-        }
-    }
+
 }
+
+
+
+
