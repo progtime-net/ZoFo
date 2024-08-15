f5d563f887330df210368bc1e5beebc9b8e6ab26
Kaktus200020 <artemkremnev2007@gmail.com>
2024-08-15 10:19:09 +0300

    addItemManagerWithUnworkMethods

---
+++ ZoFo/GameCore/GameManagers/ItemManager/ItemInfo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZoFo.GameCore.GameManagers.ItemManager
+{
+    class ItemInfo
+    {
+        //поля
+        string tag;
+        string textureName;
+        Texture2D itemTexture;
+        bool isCraftable;
+        Dictionary<string, int> resourcesNeededToCraft;
+        //методы
+        private void LoadTexture()
+        {
+            //я что-то хз как это
+        }
+    }
+}
---
+++ ZoFo/GameCore/GameManagers/ItemManager/ItemManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZoFo.GameCore.GameManagers.ItemManager
+{
+    class ItemManager
+    {
+        //поля
+        Dictionary<string, ItemInfo> tagItemPairs;
+        //методы
+        ItemInfo GetItemInfo(string tag)
+        {
+            return tagItemPairs.GetValueOrDefault(tag);
+        }
+        void LoadItemTexture()
+        {
+
+        }
+        
+    }
+}
