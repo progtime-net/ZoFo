74d5833ac684b14ff39de57138747579a11f8e8e
unknown <dimasima2908@gmail.com>
2024-08-15 14:59:29 +0300

    ent

--- ZoFo/GameCore/GameObjects/Entity.cs
+++ ZoFo/GameCore/GameObjects/Entity.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Numerics;
 
 namespace ZoFo;
 
 public class Entity
 {
-
+    Vector2 position;
+    public Entity(Vector2 position)
+    {
+        this.position = position;
+    }
+    public virtual void SetPosition(Vector2 position)
+    {
+         Vector2 pos = position; 
+    }
 }
