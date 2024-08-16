866f87c0d08a498178b805a3a9195670685df3c1
SergoDobro <optimgrey@yandex.ru>
2024-08-16 13:07:27 +0300

    GameObject Almost Finished

---
+++ ZoFo/GameCore/GameObjects/MapObjects/StopObjects/StopObject.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using ZoFo.GameCore.ZoFo_graphics;
+
+namespace ZoFo.GameCore.GameObjects.MapObjects.StopObjects;
+
+public abstract class StopObject : GameObject
+{
+    protected StopObject(Vector2 position) : base(position)
+    {
+        //TODO
+    }
+}
