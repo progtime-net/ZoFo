46b829826eb7ad0eea73a762c97f5766764ff5f6
rawer470 <timont60@bk.ru>
2024-08-20 17:58:02 +0300

    UpdateGotData

--- ZoFo/GameCore/GameManagers/NetworkManager/Updates/ServerToClient/UpdateCreatePlayer.cs
+++ ZoFo/GameCore/GameManagers/NetworkManager/Updates/ServerToClient/UpdateCreatePlayer.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZoFo.GameCore.GameManagers.NetworkManager.SerializableDTO;
 
 namespace ZoFo.GameCore.GameManagers.NetworkManager.Updates.ServerToClient
 {
     public class UpdateCreatePlayer : UpdateData
     {
         public int PlayerId {  get; set; }
+        public SerializableVector2 position { get; set; }
         public UpdateCreatePlayer()
         {
             isImportant = true;
