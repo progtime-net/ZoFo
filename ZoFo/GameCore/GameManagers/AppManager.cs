6e4235e48dece1a3b08dd4229cb9facb8e115615
SergoDobro <optimgrey@yandex.ru>
2025-08-09 01:11:55 +0300

    Init Snake Branch

--- ZoFo/GameCore/GameManagers/AppManager.cs
+++ ZoFo/GameCore/GameManagers/AppManager.cs
@@ -24,7 +24,7 @@ namespace ZoFo.GameCore.GameManagers
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-
+         
 
         public static AppManager Instance { get; private set; }
         public GameState gamestate;
